58495e21b52ce77cafd598da43d6ca7aeb38ab1b
Soham Katdare <sohamkatdare8@gmail.com>
2025-01-23 22:54:06 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Healthcare from "./Healthcare";
+import "./index.css";
+
+function App() {
+  return <Healthcare />;
+}
+
+export default App;
---
+++ src/Healthcare.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./healthcare.css";
+
+const Healthcare = () => {
+  return <div className="healthcare">Place holder</div>;
+};
+
+export default Healthcare;
